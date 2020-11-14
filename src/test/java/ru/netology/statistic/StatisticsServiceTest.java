41699e4ab33367945fa3861d52af63e080b17d44
Daria <dsnaydina@gmail.com>
2020-11-14 01:00:39 +0100

    test is added to get 100% coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,15 +17,15 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
-
   @Test
-  void findEquals () {
+  void findPreLastMax() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+    long[] incomesInBillions = {1, 2, 3, 4, 5, 6, 7, 8, 10, 9};
+    long expected = 10;
 
-    long actual = 12;
+    long actual = service.findMax(incomesInBillions);
+
     assertEquals(expected, actual);
   }
 }
